8b35e52d22eba30925f45f4dae62b3ec6dba83de
Damian Goraj <damiangoraj@gmail.com>
2020-10-29 19:04:10 +0100

    First commit. Set up MenuBar and Routing for sites.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MenuBar from './MenuBar';
+import CovidShop from "./CovidShop";
+import CovidFeed from "./CovidFeed";
+import CovidNews from "./CovidNews";
+import Login from "./Login";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Home = () => (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+    return (
+        <Router>
+            <div>
+                <MenuBar/>
+                <Switch>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/feed" component={CovidFeed}></Route>
+                    <Route path="/news" component={CovidNews}></Route>
+                    <Route path="/shop" component={CovidShop}></Route>
+                    <Route path="/login" component={Login}></Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+
+
 }
 
 export default App;
